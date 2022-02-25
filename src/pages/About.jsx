223380a91b70d8566bfb791e5ddcdc4498aac1f6
David Blatt <davidblatt10@gmail.com>
2022-02-24 22:52:17 -0500

    grammar change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ function About() {
     <>
       <h1 className="margin-bottom-1">About</h1>
       <p>
-        This website was made using React. All the CSS has been created without
-        any CSS frameworks using Sass.
+        This website was made using React. All the CSS has been created using
+        Sass without any CSS frameworks .
       </p>
       <p className="margin-top-1">
         I have tried to make the code as clean as possible. It can be viewed on{' '}
