94d4fa4c94722fd354af2ca309bd41e9efc17fd1
David Blatt <davidblatt10@gmail.com>
2022-03-17 10:13:24 -0400

    small text changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,13 @@ function Home() {
     {
       img: 'https://www.pngitem.com/pimgs/m/476-4763739_red-apex-legends-logo-png-hd-pngbg-poster.png',
       title: 'Apex Legends Data Tracker',
-      p: 'Website that displays stats and info for the game Apex Legends. Users can access up-to-date game info and player stats',
+      p: 'Website that displays stats and info for the game Apex Legends. Users can access up-to-date game info and player stats. Uses React, Tailwind, daisyUI, Axios, Context, and Reducer.',
       link: 'https://flamboyant-leakey-fe8698.netlify.app/',
     },
     {
       img: 'https://play-lh.googleusercontent.com/HUuQc4Zpl6x7fUyX-jFMmcuUbW77REw4UKl5rfhHfP4VY6ctBU1w1I_RZWsXaojFgIo',
       title: 'Todo Website',
-      p: 'A simple Todo project. Its my first attempt creating a website using the MERN stack. Its also my first time using Material UI.',
+      p: 'A simple Todo project. Its my first attempt creating a website using the MERN stack. Its also my first time using Material UI. Uses MERN Stack, Material UI, Axios, Redux.',
       link: 'https://todoapp9dnsb.herokuapp.com/',
     },
   ]
