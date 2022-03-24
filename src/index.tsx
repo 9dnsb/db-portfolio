960d841675db157b9a24c483352b9425e653eb85
David Blatt <davidblatt10@gmail.com>
2022-03-24 09:27:10 -0400

    updated patches and minor versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.min.css'
+import 'devicon/devicon.min.css'
 import App from './App'
 
 ReactDOM.render(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,6 +36,32 @@ function Home() {
           <Project project={project} key={index} />
         ))}
       </div>
+      {/* <div className="iconSize">
+        <i className="devicon-css3-plain"></i>
+        <i className="devicon-express-original"></i>
+        <i className="devicon-git-plain"></i>
+        <i className="devicon-github-original"></i>
+        <i className="devicon-heroku-original"></i>
+        <i className="devicon-html5-plain"></i>
+        <i className="devicon-javascript-plain"></i>
+        <i className="devicon-jest-plain"></i>
+        <i className="devicon-materialui-plain"></i>
+        <i className="devicon-markdown-original"></i>
+        <i className="devicon-mongodb-plain"></i>
+        <i className="devicon-nextjs-original"></i>
+        <i className="devicon-nodejs-plain"></i>
+        <i className="devicon-npm-original-wordmark"></i>
+        <i className="devicon-pandas-original"></i>
+        <i className="devicon-python-plain"></i>
+        <i className="devicon-react-original"></i>
+        <i className="devicon-redux-original"></i>
+        <i className="devicon-sass-original"></i>
+        <i className="devicon-spss-plain"></i>
+        <i className="devicon-swift-plain"></i>
+        <i className="devicon-tailwindcss-original-wordmark"></i>
+        <i className="devicon-typescript-plain"></i>
+        <i className="devicon-vscode-plain"></i>
+      </div> */}
     </>
   )
 }
