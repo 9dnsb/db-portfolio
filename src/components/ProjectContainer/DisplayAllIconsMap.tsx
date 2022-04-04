d4ee402c8772b8673febce9c7bec1396435036d9
David Blatt <davidblatt10@gmail.com>
2022-04-04 09:25:36 -0400

    fixed color

--- src/components/ProjectContainer/DisplayAllIconsMap.tsx
+++ src/components/ProjectContainer/DisplayAllIconsMap.tsx
@@ -3,13 +3,18 @@ import { iconArray } from '../../types/IconGridType'
 
 function DisplayAllIconsMap() {
   return (
-    <div className="iconGrid margin-top-3">
-      {iconArray.map((icon) => (
-        <div key={icon.icon} className="">
-          <i className={icon.icon}></i>
-          <p>{icon.name}</p>
-        </div>
-      ))}
+    <div className="card" id="skillsCard">
+      <h2 className="margin-top-5 margin-left-2 margin-right-2">
+        The skills, tools and technologies I use to bring your products to life:
+      </h2>
+      <div className="iconGrid margin-top-3">
+        {iconArray.map((icon) => (
+          <div key={icon.icon} className="">
+            <i className={icon.icon}></i>
+            <p>{icon.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,15 @@ function Home() {
   return (
     <div id="mainPage">
       <div id="main-page-heading">
-        <h1 className="margin-top-2">David Blatt's Portfolio Website</h1>
+        <div className="card">
+          <h1 className="margin-top-2 portHeader">
+            David Blatt's Portfolio Website
+          </h1>
+        </div>
       </div>
-      <h2 className="margin-top-5">
-        The skills, tools and technologies I use to bring your products to life:
-      </h2>
+
       <DisplayAllIconsMap />
-      <h2 className="margin-top-5">
+      <h2 className="margin-top-5 portHeader">
         View some of the project's I've been working on:
       </h2>
       <DisplayAllProjectsMap />
